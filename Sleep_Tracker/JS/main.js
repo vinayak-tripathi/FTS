65985b4db1ab9bf9c27af0290dbd9ad7ef8e84c4
Vinayak Tripathi <66787122+vinayak-tripathi@users.noreply.github.com>
2021-07-28 20:25:18 +0530

    Note making app done

---
+++ Sleep_Tracker/JS/main.js
@@ -0,0 +1,33 @@
+var details, count = 0;
+
+function show(title, note) {
+    if (count>6){
+        alert("7 days data is enough");
+    }
+    else{
+        details.append('<tr>'+
+                    '<td><input type="date" name="date" id="da'+count+'" class="da"></td>'+
+                    '<td><input type="time" name="st" id="st'+count+'" class="st"></td>'+
+                    '<td><input type="time" name="wt" id="wt'+count+'" class="wt"></td>'+
+                    '<td><img class= "close" src="images/close.png"/></td>'+
+                    '</tr>');
+        var last = details.find("tr:last");
+        last.find("img").click(function () {
+            last.remove();
+        });
+        count+=1;
+    }
+
+    console.log(count);
+}
+
+function analyse(){
+    console.log(count);
+    var list = [];
+    for(var  i=0;i<count;i++){
+        console.log(document.getElementById("da"+i).value);
+        console.log(document.getElementById("st"+i).value);
+        console.log(document.getElementById("wt"+i).value);
+    }
+    
+}
